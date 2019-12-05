pevik/qa_test_klp

commit f5e7bad14bc505167c39a1f8284b630793446d4d

Adapt the livepatch template to a new livepatch API
The atomic replace patch set among others removed the two-stage API.
There is no (un)registration step needed now. SLES backport defines
KLP_NOREG_API macro to easily distinguish whether the kernel provides
the old or the new API. Use it and change the module init function of
the template accordingly.

Signed-off-by: Miroslav Benes <mbenes@suse.cz>

klp_test_livepatch.c
@@ -67,6 +67,7 @@ static struct klp_patch patch = {
 
 static int livepatch_init(void)
 {
+#ifndef KLP_NOREG_API
 	int ret;
 
 	ret = klp_register_patch(&patch);
@@ -78,11 +79,16 @@ static int livepatch_init(void)
 		return ret;
 	}
 	return 0;
+#else
+	return klp_enable_patch(&patch);
+#endif
 }
 
 static void livepatch_exit(void)
 {
+#ifndef KLP_NOREG_API
 	WARN_ON(klp_unregister_patch(&patch));
+#endif
 }
 
 module_init(livepatch_init);

commit fde5db52ea3465c5848a6c4fe2bad6fbdef2c836

Fix old registration API detection
Detect whether functions from old API exists.

Depending on SLES specific KLP_NOREG_API does not work on Tumbleweed
and mainline.

We could employ autotools, but current code style.

Fixes: f5e7bad Adapt the livepatch template to a new livepatch API

Signed-off-by: Petr Vorel <pvorel@suse.cz>

klp_tc_functions.sh
@@ -28,7 +28,8 @@ function __klp_add_patched_func() {
 }
 
 function klp_create_header() {
-    sed "s%@@USE_OLD_HRTIMER_API@@%$KLP_TEST_HRTIMER_OLD%" \
+    sed -e "s%@@USE_OLD_HRTIMER_API@@%$KLP_TEST_HRTIMER_OLD%" \
+	    -e "s%@@USE_OLD_REG_API@@%$KLP_TEST_REG_API%" \
 	    "${SOURCE_DIR}/klp_test_support_mod.h" \
 	    > "${OUTPUT_DIR}/klp_test_support_mod.h"
 }
@@ -305,6 +306,14 @@ if [ ! -f $KLP_ENV_CACHE_FILE ]; then
 
     echo -n 'export KLP_TEST_HRTIMER_OLD=' > $KLP_ENV_CACHE_FILE
     if klp_compile_module $COMPILETEST_DIR/klp_compile_test_hrtimer.c > /dev/null 2>&1;
+    then
+        echo "1" >> $KLP_ENV_CACHE_FILE
+    else
+        echo "0" >> $KLP_ENV_CACHE_FILE
+    fi
+
+    echo -n 'export KLP_TEST_REG_API=' >> $KLP_ENV_CACHE_FILE
+    if grep klp_register_patch "/lib/modules/$(uname -r)/source/include/linux/livepatch.h"
     then
         echo "1" >> $KLP_ENV_CACHE_FILE
     else

klp_test_livepatch.c
@@ -1,7 +1,7 @@
 /*
  * klp_test_livepatch - test livepatch template
  *
- *  Copyright (c) 2017-2018 SUSE
+ *  Copyright (c) 2017-2019 SUSE
  *   Authors: Libor Pechacek, Nicolai Stange
  */
 
@@ -67,7 +67,7 @@ static struct klp_patch patch = {
 
 static int livepatch_init(void)
 {
-#ifndef KLP_NOREG_API
+#if USE_OLD_REG_API
 	int ret;
 
 	ret = klp_register_patch(&patch);
@@ -86,7 +86,7 @@ static int livepatch_init(void)
 
 static void livepatch_exit(void)
 {
-#ifndef KLP_NOREG_API
+#if USE_OLD_REG_API
 	WARN_ON(klp_unregister_patch(&patch));
 #endif
 }

klp_test_support_mod.h
@@ -1,7 +1,7 @@
 /*
  * klp_test_support_mod - support module for KLP testing
  *
- *  Copyright (c) 2018 SUSE
+ *  Copyright (c) 2018-2019 SUSE
  *   Author: Nicolai Stange
  *
  * This program is free software; you can redistribute it and/or modify it
@@ -21,6 +21,7 @@
 #include <asm/uaccess.h>
 
 #define USE_OLD_HRTIMER_API @@USE_OLD_HRTIMER_API@@
+#define USE_OLD_REG_API @@USE_OLD_REG_API@@
 
 #if defined(PATCH_ID)
 #define __PATCHED_SYM(id, sym) klp_ ## id ## _ ## sym